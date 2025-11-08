97eef48b426c49c64b18cfe8d773754b4919bf2a
tparida3 <tparida3@gatech.edu>
2025-11-08 15:55:29 -0500

    phone works

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    allowedHosts: [
+        'annamarie-inoxidizable-nonperpetually.ngrok-free.dev'
+    ],
     proxy: {
       '/api': {
         target: 'http://localhost:4000',
